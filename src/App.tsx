928961071f4f6620f3bab918e0379814536337e9
vaibhav-singh-01 <vaibhav.singh01@telusinternational.com>
2025-07-07 00:58:24 +0530

    simplified

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 
 const App = () => {
     return (
         <div className='app'>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-            </Routes>
+            <Home />
         </div>
     );
 };
